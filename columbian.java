b0790abf9f3136d7c0434f4964a286264decad79
Justin Lowen <Justin.G.Lowen@gmail.com>
2021-01-22 15:12:05 -0600

    Simple calculator

--- columbian.java
+++ columbian.java
@@ -3,15 +3,12 @@ import java.util.Scanner;
 class apples {
     public static void main(String args[]) {
         Scanner justin = new Scanner(System.in);
-        System.out.println("Hello apples!");
-        double tuna;
-        tuna = 5.28;
-
-        System.out.print("I want ");
-        System.out.print(tuna);
-        System.out.print(" movies.");
-        System.out.println("Use the scanner I have granted thee:");
-
-        System.out.println(justin.nextLine());
+        double fnum, snum, answer;
+        System.out.println("Enter first num: ");
+        fnum = justin.nextDouble();
+        System.out.println("Enter second num: ");
+        snum = justin.nextDouble();
+        answer = fnum + snum;
+        System.out.println(answer);
     }
 }
