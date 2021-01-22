32a353dfc8873ba2fa53210090d389b24f7834db
Justin Lowen <Justin.G.Lowen@gmail.com>
2021-01-22 15:05:01 -0600

    First steps

---
+++ columbian.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class apples {
+    public static void main(String args[]) {
+        Scanner justin = new Scanner(System.in);
+        System.out.println("Hello apples!");
+        double tuna;
+        tuna = 5.28;
+
+        System.out.print("I want ");
+        System.out.print(tuna);
+        System.out.print(" movies.");
+        System.out.println("Use the scanner I have granted thee:");
+
+        System.out.println(justin.nextLine());
+    }
+}
