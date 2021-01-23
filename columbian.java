afbb44c2337ceb6b61398055bae94445f111fa9a
Justin Lowen <Justin.G.Lowen@gmail.com>
2021-01-22 20:20:07 -0600

    Nested if else example

--- columbian.java
+++ columbian.java
@@ -2,9 +2,20 @@ import java.util.Scanner;
 
 class columbian {
     public static void main(String args[]) {
-        sumatra sumatraObject = new sumatra("Joelle");
-        sumatraObject.saying();
+        int age = 110;
 
-
+        if (age < 50) {
+            System.out.println("You are young.");
+        } else if (age >= 50) {
+            System.out.println("You have experience.");
+            if (age > 75 || age <= 100) {
+                System.out.println("You have wisdom.");
+            } else {
+                System.out.println("You are somewhere in between.");
+            }
+        } else {
+            System.out.println("Wow, I didn't expect that age!");
         }
+
+    }
 }
