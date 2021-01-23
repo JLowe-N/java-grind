c63cdb3692603794424f4a6d53895c75ff295bd6
Justin Lowen <Justin.G.Lowen@gmail.com>
2021-01-23 13:57:54 -0600

    Constructor overrides example

--- columbian.java
+++ columbian.java
@@ -3,9 +3,13 @@ import java.util.Random;
 class columbian {
     public static void main(String[] args) {
         sumatra sumatraObject = new sumatra();
-        System.out.println(sumatraObject.toMilitary());
-        sumatraObject.setTime(13, 27, 6);
-        System.out.println(sumatraObject.toMilitary());
-        System.out.println(sumatraObject.toString());
+        sumatra sumatraObject2 = new sumatra(5);
+        sumatra sumatraObject3 = new sumatra(5, 13);
+        sumatra sumatraObject4 = new sumatra(5, 13, 43);
+
+        System.out.printf("%s\n", sumatraObject.toMilitary());
+        System.out.printf("%s\n", sumatraObject2.toMilitary());
+        System.out.printf("%s\n", sumatraObject3.toMilitary());
+        System.out.printf("%s\n", sumatraObject4.toMilitary());
     }
 }
--- sumatra.java
+++ sumatra.java
@@ -3,18 +3,54 @@ public class sumatra {
     private int minute;
     private int second;
 
+    public sumatra() {
+        this(0, 0, 0);
+    }
+
+    public sumatra(int h) {
+        this(h, 0, 0);
+    }
+
+    public sumatra(int h, int m) {
+        this(h, m, 0);
+    }
+
+    public sumatra(int h, int m, int s) {
+        setTime(h, m, s);
+    }
+
     public void setTime(int h, int m, int s) {
+        setHour(h);
+        setMinute(m);
+        setSecond(s);
+    }
+
+    public void setHour(int h) {
         hour = ((h >= 0 && h < 24) ? h : 0);
+    }
+
+    public void setMinute(int m) {
         minute = ((m >= 0 && m < 60) ? m : 0);
+    }
+
+    public void setSecond(int s) {
         second = ((s >= 0 && s < 60) ? s : 0);
     }
 
-    public String toMilitary() {
-        return String.format("%02d:%02d:%02d", hour, minute, second);
+    public int getHour() {
+        return hour;
     }
 
-    public String toString() {
-        return String.format("%d:%02d:%02d %s", ((hour == 0 || hour == 12) ? 12 : hour % 12), minute, second,
-                hour < 12 ? "AM" : "PM");
+    public int getMinute() {
+        return minute;
     }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public String toMilitary() {
+        return String.format("%02d:%02d:%02d", getHour(), getMinute(), getSecond());
+    }
+
 }
