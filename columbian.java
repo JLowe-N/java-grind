02c4392c6e8b9e15a0dd3009ab97fa9f89505268
Justin Lowen <Justin.G.Lowen@gmail.com>
2021-01-22 21:05:53 -0600

    Iterate through array

--- columbian.java
+++ columbian.java
@@ -4,10 +4,14 @@ class columbian {
     public static void main(String args[]) {
         System.out.println("Index\tValue");
         int justin[]={32,12,18,54,2};
+        int sum=0;
 
         for (int counter=0; counter<justin.length; counter++) {
+            sum += justin[counter];
             System.out.println(counter + "\t" + justin[counter]);
         }
+
+        System.out.println("Sum is " + sum);
 
     }
 }
