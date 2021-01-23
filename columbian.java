868f320212870cc8f80a9b0576f346d0f5a7a1df
Justin Lowen <Justin.G.Lowen@gmail.com>
2021-01-22 21:56:31 -0600

    Variable length argument method

--- columbian.java
+++ columbian.java
@@ -2,20 +2,15 @@ import java.util.Random;
 
 class columbian {
     public static void main(String args[]) {
-        int firstarray[][] = { { 8, 9, 10, 11 }, { 12, 13, 14, 15 } };
-        int secondarray[][] = { { 2, 3, 4 }, { 1 }, { 21, 7, 7 } };
-
-        display(firstarray);
-        System.out.print("\n\n");
-        display(secondarray);
+        System.out.println(average(1,2,3,4,5,6,7,8,9,10))
     }
 
-    public static void display(int x[][]) {
-        for (int row = 0; row < x.length; row++) {
-            for (int column = 0; column < x[row].length; column++) {
-                System.out.print(x[row][column] + "\t");
-            }
-            System.out.print("\n");
-        }
+    public static int average(int...numbers) {
+        int total=0;
+        for (int x:numbers) 
+            total += x;
+        
+
+        return total/numbers.length;
     }
 }
