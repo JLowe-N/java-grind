771a54973516cf6c522d8a9b564b39b84a3977dc
Justin Lowen <Justin.G.Lowen@gmail.com>
2021-01-23 13:39:18 -0600

    12-hour clock method

--- columbian.java
+++ columbian.java
@@ -6,5 +6,6 @@ class columbian {
         System.out.println(sumatraObject.toMilitary());
         sumatraObject.setTime(13, 27, 6);
         System.out.println(sumatraObject.toMilitary());
+        System.out.println(sumatraObject.toString());
     }
 }
