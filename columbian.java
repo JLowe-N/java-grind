76a46fb8bdd2d4c450eb36f60cdc44a789b6021f
Justin Lowen <Justin.G.Lowen@gmail.com>
2021-01-23 14:05:25 -0600

    Reserved to string method for class print calls

--- columbian.java
+++ columbian.java
@@ -2,14 +2,6 @@ import java.util.Random;
 
 class columbian {
     public static void main(String[] args) {
-        sumatra sumatraObject = new sumatra();
-        sumatra sumatraObject2 = new sumatra(5);
-        sumatra sumatraObject3 = new sumatra(5, 13);
-        sumatra sumatraObject4 = new sumatra(5, 13, 43);
-
-        System.out.printf("%s\n", sumatraObject.toMilitary());
-        System.out.printf("%s\n", sumatraObject2.toMilitary());
-        System.out.printf("%s\n", sumatraObject3.toMilitary());
-        System.out.printf("%s\n", sumatraObject4.toMilitary());
+        costarican costaricanObject = new costarican(4, 5, 6);
     }
 }
