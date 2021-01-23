a50e50d86c3a38eca28318c30b4e992274fbeca6
Justin Lowen <Justin.G.Lowen@gmail.com>
2021-01-23 13:27:52 -0600

    Working with times

--- columbian.java
+++ columbian.java
@@ -1,16 +1,10 @@
 import java.util.Random;
 
 class columbian {
-    public static void main(String args[]) {
-        System.out.println(average(1,2,3,4,5,6,7,8,9,10))
-    }
-
-    public static int average(int...numbers) {
-        int total=0;
-        for (int x:numbers) 
-            total += x;
-        
-
-        return total/numbers.length;
+    public static void main(String[] args) {
+        sumatra sumatraObject = new sumatra();
+        System.out.println(sumatraObject.toMilitary());
+        sumatraObject.setTime(13, 27, 6);
+        System.out.println(sumatraObject.toMilitary());
     }
 }
--- sumatra.java
+++ sumatra.java
@@ -1,16 +1,15 @@
 public class sumatra {
-    private String girlName;
+    private int hour;
+    private int minute;
+    private int second;
 
-    public sumatra(String name) {
-        girlName = name;
+    public void setTime(int h, int m, int s) {
+        hour = ((h >= 0 && h < 24) ? h : 0);
+        minute = ((m >= 0 && m < 60) ? m : 0);
+        second = ((s >= 0 && s < 60) ? s : 0);
     }
-    public void setName(String name){
-        girlName = name;
-    }
-    public String getName(){
-        return girlName;
-    }
-    public void saying() {
-        System.out.printf("Your favorite person is %s", getName());
+
+    public String toMilitary() {
+        return String.format("%02d:%02d:%02d", hour, minute, second);
     }
 }
