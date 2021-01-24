f8ff50606f43e2f27b72cfa5fae23e04ca4835dc
Justin Lowen <Justin.G.Lowen@gmail.com>
2021-01-24 11:40:39 -0600

    Try basic algorithm

---
+++ algorithms/twonumbersum/programtest.java
@@ -0,0 +1,21 @@
+package algorithms.twonumbersum;
+
+import java.util.*;
+
+class ProgramTest {
+    @Test
+    public void TestCase1() {
+        int[] output = Program.twoNumberSum(new int[] { 3, 5, -4, 8, 11, 1, -1, 6 }, 10);
+        Utils.assertTrue(output.length == 2);
+        Utils.assertTrue(contains(output, -1));
+        Utils.assertTrue(contains(output, 11));
+    }
+
+    public boolean contains(int[] output, int val) {
+        for (var el : output) {
+            if (el == val)
+                return true;
+        }
+        return false;
+    }
+}
