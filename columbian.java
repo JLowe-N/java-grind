bcabdfac8cc313ede10066b4328f4e213015cf97
Justin Lowen <Justin.G.Lowen@gmail.com>
2021-01-23 22:56:26 -0600

    Basic form with action handlers

--- columbian.java
+++ columbian.java
@@ -2,9 +2,9 @@ import javax.swing.JFrame;
 
 class columbian {
     public static void main(String[] args) {
-        sumatra justin = new sumatra();
-        justin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        justin.setSize(275, 180);
-        justin.setVisible(true);
+        sumatra application = new sumatra();
+        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        application.setSize(350, 100);
+        application.setVisible(true);
     }
 }
