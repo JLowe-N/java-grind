4cc190b7c834f1f58bc82a09591660ce2548feb3
Justin Lowen <Justin.G.Lowen@gmail.com>
2021-01-23 18:50:26 -0600

    EnumSet Example

--- columbian.java
+++ columbian.java
@@ -1,10 +1,14 @@
 import java.util.Random;
+import java.util.EnumSet;
 
 class columbian {
     public static void main(String[] args) {
-        costarican costaricanObject = new costarican(1, 1, 2020);
-        sumatra sumatraObject = new sumatra("Justin", costaricanObject);
+        for (sumatra people : sumatra.values())
+            System.out.printf("%s\t%s\t%s\n", people, people.getDesc(), people.getYear());
 
-        System.out.println(sumatraObject);
+        System.out.println("\nAnd now for the range of constants!!!\n");
+
+        for (sumatra people : EnumSet.range(sumatra.kelsey, sumatra.sruthi))
+            System.out.printf("%s\t%s\t%s\n", people, people.getDesc(), people.getYear());
     }
 }
--- sumatra.java
+++ sumatra.java
@@ -1,14 +1,20 @@
-public class sumatra {
-    private String name;
-    private costarican birthday;
+public enum sumatra {
+    justin("cool", "28"), bucky("nice", "22"), jules("worker", "33"), kelsey("smart", "22"),
+    sruthi("adventurous", "24");
 
-    public sumatra(String theName, costarican theDate) {
-        name = theName;
-        birthday = theDate;
+    private final String desc;
+    private final String year;
+
+    sumatra(String description, String birthday) {
+        desc = description;
+        year = birthday;
     }
 
-    public String toString() {
-        return String.format("My name is %s, my birthday is %s", name, birthday);
+    public String getDesc() {
+        return desc;
     }
 
+    public String getYear() {
+        return year;
+    }
 }
